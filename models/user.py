f5327065088e13436c07874258523e8063c1db44
mins8578 <mins8578@naver.com>
2026-02-05 16:05:17 +0900

    refactor: backend. 제거 (디렉터리 위치 변경)

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from backend.database import Base
+from database import Base
 
 
 class User(Base):
