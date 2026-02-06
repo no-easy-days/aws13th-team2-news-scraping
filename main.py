24e35f3a32c70cb68496101270bd6e9deac617ee
Eunice(Eunyoung) Shin <eunice.shin54@gmail.com>
2026-02-06 13:41:40 +0900

    fix: remove unnecessary code

--- main.py
+++ main.py
@@ -43,19 +43,6 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-Base.metadata.create_all(bind=engine)
-
-app.include_router(articles.router)
-app.include_router(bookmark_router)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React 앱 주소
-    allow_credentials=True,
     allow_methods=["*"],  # 모든 HTTP 메서드 허용
     allow_headers=["*"],  # 모든 헤더 허용
 )
